1fc886b1455fe63bec1a266a7f50836445c53cb5
joaobiriba <glaquidara@gmail.com>
2014-08-05 22:05:12 +0200

    Creation of BaasBox Dart SDK

---
+++ test/baasbox_test.dart
@@ -0,0 +1,12 @@
+import 'package:baasbox/baasbox.dart';
+
+
+void main() {
+  BaasBox bb = new BaasBox();
+  bb.setEndPoint("http://localhost:9000");
+  bb.appcode = "1234567890";
+  
+  bb.login('test', 'test');
+  
+  
+}
