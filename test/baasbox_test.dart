84eed46d0afff248af3511d56dff0ecfe130fdb1
joaobiriba <glaquidara@gmail.com>
2014-08-08 16:46:46 +0200

    Adding unittest

--- test/baasbox_test.dart
+++ test/baasbox_test.dart
@@ -1,11 +1,39 @@
 import 'package:baasbox/baasbox.dart';
-
+import 'package:unittest/unittest.dart';
+import 'package:unittest/html_config.dart';
 
 void main() {
-  BaasBox bb = new BaasBox();
-  bb.setEndPoint("http://localhost:9000");
-  bb.appcode = "1234567890";
+  useHtmlConfiguration();
+
+  BaasBox bb;
+
+  group('login future', () {
+    setUp(() {
+      bb = new BaasBox();
+      bb.setEndPoint("http://localhost:9000");
+      bb.appcode = "1234567890";
+    });
+
+    test('login', () {
+      var future = bb.login('test', 'test');
+      expect(future, completion(true));
+    });
+  });
+
   
-  bb.login('test', 'test').then((value) => bb.logout());
   
+
+  group('login future', () {
+    setUp(() {
+      bb = new BaasBox();
+      bb.setEndPoint("http://localhost:9000");
+      bb.appcode = "1234567890";
+      return bb.login('test', 'test').then((_) => print(bb.user['username']));
+                
+    });
+
+    test('login ok', () {
+      expect(bb.user['username'], equals('test') );
+    });
+  });
 }
