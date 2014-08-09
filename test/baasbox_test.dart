70ebd37065564bdea16e19e7c378d22a24f9fbd0
joaobiriba <glaquidara@gmail.com>
2014-08-09 14:32:29 +0200

    Right testing of login api

--- test/baasbox_test.dart
+++ test/baasbox_test.dart
@@ -1,6 +1,7 @@
 import 'package:baasbox/baasbox.dart';
 import 'package:unittest/unittest.dart';
 import 'package:unittest/html_config.dart';
+import 'dart:async';
 
 void main() {
   useHtmlConfiguration();
@@ -16,24 +17,39 @@ void main() {
 
     test('login', () {
       var future = bb.login('test', 'test');
-      expect(future, completion(true));
+      expect(future, completes);
     });
   });
 
-  
-  
-
-  group('login future', () {
+  group('login future right user', () {
     setUp(() {
       bb = new BaasBox();
       bb.setEndPoint("http://localhost:9000");
       bb.appcode = "1234567890";
-      return bb.login('test', 'test').then((_) => print(bb.user['username']));
-                
     });
 
     test('login ok', () {
-      expect(bb.user['username'], equals('test') );
+      Future<Map> future = bb.login('test', 'test');
+        expect(future, completion( containsPair('result', 'ok') ));
+        //expect(future, completion( containsPair("name", 'test') ));
+      expect(future, completes);
+
     });
   });
+  
+  group('login future wrong user', () {
+     setUp(() {
+       bb = new BaasBox();
+       bb.setEndPoint("http://localhost:9000");
+       bb.appcode = "1234567890";
+     });
+
+     test('login not ok', () {
+       Future<Map> future = bb.login('testo', 'testa');
+         expect(future, completion( containsPair('result', 'error') ));
+         //expect(future, completion( containsPair("name", 'test') ));
+       expect(future, completes);
+
+     });
+   });
 }
