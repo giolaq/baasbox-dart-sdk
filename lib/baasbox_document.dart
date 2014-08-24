7efa18ab18840afeb6c7be57bd8eab12af4fbdd9
joaobiriba <glaquidara@gmail.com>
2014-08-24 23:42:28 +0200

    Added retrieve, modify, delete, count API to BaasBoxDocument to test

--- lib/baasbox_document.dart
+++ lib/baasbox_document.dart
@@ -8,7 +8,7 @@ class BaasBoxDocument {
   String creation_date;
   String rid;
   double version;
-  
+
 
   Future create(String collection, Map data) {
 
@@ -16,7 +16,50 @@ class BaasBoxDocument {
 
     BaasBoxContext bbCtxt = new BaasBoxContext();
 
-    ftr = bbCtxt.rest("POST", '/document/' + collection , data, true);
+    ftr = bbCtxt.rest("POST", '/document/' + collection, data, true);
+    return ftr;
+  }
+
+  Future retrieve(String documentId, String collection) {
+
+    Map data;
+    Future ftr;
+
+    BaasBoxContext bbCtxt = new BaasBoxContext();
+
+    ftr = bbCtxt.rest("GET", '/document/' + collection + '/' + documentId, data, true);
+    return ftr;
+  }
+
+  Future modify(String documentId, String collection, Map data) {
+
+    Future ftr;
+
+    BaasBoxContext bbCtxt = new BaasBoxContext();
+
+    ftr = bbCtxt.rest("PUT", '/document/' + collection + '/' + documentId, data, true);
+    return ftr;
+  }
+
+  Future delete(String documentId, String collection) {
+
+    Map data;
+    Future ftr;
+
+    BaasBoxContext bbCtxt = new BaasBoxContext();
+
+    ftr = bbCtxt.rest("DELETE", '/document/' + collection + '/' + documentId, data, true);
+    return ftr;
+  }
+
+  Future count(String collection) {
+
+    Map data;
+    Future ftr;
+
+    BaasBoxContext bbCtxt = new BaasBoxContext();
+
+    ftr = bbCtxt.rest("GET", '/document/' + collection + '/count', data, true);
     return ftr;
   }
 }
