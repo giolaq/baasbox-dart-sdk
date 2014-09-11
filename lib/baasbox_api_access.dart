56d485579aaaf4e5cb59e0cd64f6b301b443a388
joaobiriba <glaquidara@gmail.com>
2014-09-11 19:11:57 +0200

    Idomatic Dart Api Access API

--- lib/baasbox_api_access.dart
+++ lib/baasbox_api_access.dart
@@ -3,46 +3,26 @@ part of baasbox;
 class BaasBoxApiAccess {
  
   Map groups;
+  BaasBoxContext _bbCtxt;
+  
+  BaasBoxApiAccess() {
+     this._bbCtxt = new BaasBoxContext();
+  }
 
   Future<Map> listGroups() {
 
     Future ftr;
    
-    BaasBoxContext bbCtxt = new BaasBoxContext();
-
-    ftr = bbCtxt.rest("GET", '/admin/endpoints', true).
+    ftr = _bbCtxt.rest("GET", '/admin/endpoints', true).
         then( (onReponse) => this.groups = onReponse['data']);
         
     return ftr;
   }
   
-  Future<Map> readGroup(String groupName) {
-
-     Future ftr;
+  Future<Map> readGroup(String groupName) => _bbCtxt.rest("GET", '/admin/endpoints/$groupName', true);         
     
-     BaasBoxContext bbCtxt = new BaasBoxContext();
+  Future<Map> enableGroup(String groupName) => _bbCtxt.rest("PUT", '/admin/endpoints/$groupName/enabled', true);           
 
-     ftr = bbCtxt.rest("GET", '/admin/endpoints/$groupName', true);         
-     return ftr;
-   }
-  
-  Future<Map> enableGroup(String groupName) {
+  Future<Map> disableGroup(String groupName) => _bbCtxt.rest("DELETE", '/admin/endpoints/$groupName/enabled', true);           
 
-       Future ftr;
-      
-       BaasBoxContext bbCtxt = new BaasBoxContext();
-
-       ftr = bbCtxt.rest("PUT", '/admin/endpoints/$groupName/enabled', true);           
-       return ftr;
-     }
-  
-  Future<Map> disableGroup(String groupName) {
-
-        Future ftr;
-       
-        BaasBoxContext bbCtxt = new BaasBoxContext();
-
-        ftr = bbCtxt.rest("DELETE", '/admin/endpoints/$groupName/enabled', true);           
-        return ftr;
-      }
 }
