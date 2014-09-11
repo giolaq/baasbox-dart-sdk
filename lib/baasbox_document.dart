b8cb067aec14a2d505e01b22264a0c0db6c5f16c
joaobiriba <glaquidara@gmail.com>
2014-09-11 19:02:36 +0200

    Idomatic Dart Document API

--- lib/baasbox_document.dart
+++ lib/baasbox_document.dart
@@ -9,83 +9,37 @@ class BaasBoxDocument {
   String rid;
   double version;
 
+  BaasBoxContext _bbCtxt = new BaasBoxContext();
+
+  BaasBoxDocument() {
+    this._bbCtxt = new BaasBoxContext();
+  }
 
   Future create(String collection, Map dataInDocument) {
 
     Future ftr;
-    
+    ftr = _bbCtxt.rest("POST", '/document/' + collection, true, dataInDocument);
 
-    BaasBoxContext bbCtxt = new BaasBoxContext();
-
-    ftr = bbCtxt.rest("POST", '/document/' + collection, true, dataInDocument);
-
-    ftr.then((documentCreated) { 
+    ftr.then((documentCreated) {
       this.data = documentCreated['data'];
       this.id = data['id'];
       this.collection = collection;
     });
-    
+
     return ftr;
   }
 
-  Future<Map> retrieve(String documentId, String collection) {
+  Future<Map> retrieve(String documentId, String collection) => _bbCtxt.rest("GET", '/document/' + collection + '/' + documentId, true);
 
-    Future ftr;
+  Future modify(String documentId, String collection, Map data) => _bbCtxt.rest("PUT", '/document/' + collection + '/' + documentId, true, data);
 
-    BaasBoxContext bbCtxt = new BaasBoxContext();
+  Future delete(String collection) => _bbCtxt.rest("DELETE", '/document/' + collection + '/' + id, true);
 
-    ftr = bbCtxt.rest("GET", '/document/' + collection + '/' + documentId, true);
-    return ftr;
-  }
+  Future<Map> count(String collection) => _bbCtxt.rest("GET", '/document/' + collection + '/count', true);
 
-  Future modify(String documentId, String collection, Map data) {
+  Future<Map> updateField(String fieldName, String documentId, String collection, Map dataToEdit) => _bbCtxt.rest("PUT", '/document/$collection/$documentId/.$fieldName', true, dataToEdit);
 
-    Future ftr;
+  Future grantPermissions(String action, String username) => _bbCtxt.rest("PUT", '/document/$collection/$id/$action/user/$username', true);
 
-    BaasBoxContext bbCtxt = new BaasBoxContext();
-
-    ftr = bbCtxt.rest("PUT", '/document/' + collection + '/' + documentId, true, data);
-    return ftr;
-  }
-
-  Future delete(String collection) {
-
-    Future ftr;
-
-    BaasBoxContext bbCtxt = new BaasBoxContext();
-
-    ftr = bbCtxt.rest("DELETE", '/document/' + collection + '/' + id, true);
-    return ftr;
-  }
-
-  Future<Map> count(String collection) {
-
-    Future ftr;
-
-    BaasBoxContext bbCtxt = new BaasBoxContext();
-
-    ftr = bbCtxt.rest("GET", '/document/' + collection + '/count', true);
-    return ftr;
-  }
-
-  Future<Map> updateField(String fieldName, String documentId, String collection, Map dataToEdit) {
-
-    Future ftr;
-
-    BaasBoxContext bbCtxt = new BaasBoxContext();
-
-    ftr = bbCtxt.rest("PUT", '/document/$collection/$documentId/.$fieldName', true, dataToEdit);
-    return ftr;
-  }
-  
-  Future grantPermissions(String action, String username) {
-
-     Future ftr;
-
-     BaasBoxContext bbCtxt = new BaasBoxContext();
-
-     ftr = bbCtxt.rest("PUT", '/document/$collection/$id/$action/user/$username', true);
-     return ftr;
-  }
 
 }
